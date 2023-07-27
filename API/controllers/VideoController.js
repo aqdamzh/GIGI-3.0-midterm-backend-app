d2f77e603c9203dbf100e52b8fc9b9973ba41aed
aqdamzh <aqdamzain1@gmail.com>
2023-07-28 04:33:32 +0700

    db - api integration: video -> crud

--- API/controllers/VideoController.js
+++ API/controllers/VideoController.js
@@ -11,24 +11,67 @@ class VideoController {
         }
     }
 
-    static getVideo(req, res) {
-        res.send('Gen an existing video');
+    static async getVideo(req, res) {
+        const videoId = req.params.videoId;
+        try {
+            const myVideo = await Video.findOne({_id: videoId});
+            res.json(myVideo);
+        } catch (error) {
+            res.status(500).json({message: error.message});
+        }
     }
 
-    static createVideo(req, res) {
-        res.send('Create a new video');
+    static async createVideo(req, res) {
+        const newVideo = new Video({
+            title: req.body.title,
+            src: req.body.src,
+            thumbnail: req.body.thumbnail
+        });
+
+        try {
+            const videoSave = await newVideo.save();
+            const {_id , title, src, thumbnail} = videoSave;
+            res.status(201).json({
+                _id: _id,
+                title: title,
+                src: src,
+                thumbnail: thumbnail
+            });
+        } catch (error) {
+            res.status(400).json({message: error.message});
+        }
     }
 
-    static updateVideo(req, res) {
-        res.send('Update an existing video');
+    static async updateVideo(req, res) {
+        const videoId = req.params.videoId;
+        const {title, src, thumbnail} = req.body;
+        try {
+            const upRes = await Video.updateOne({_id: videoId}, {
+                title: title, src: src, thumbnail: thumbnail
+            });
+            if(upRes.matchedCount>0){
+                const myVideo = await Video.findOne({_id: videoId});
+                res.json(myVideo);
+            }else {
+                res.status(404).json({message: "Id not found"});
+            }
+        } catch (error) {
+            res.status(500).json({message: error.message});
+        }
     }
 
-    static deleteVideo(req ,res) {
-        res.send('Delete an existing video');
-    }
-
-    static addProduct(req ,res) {
-        res.send('Add product to the video');
+    static async deleteVideo(req ,res) {
+        const videoId = req.params.videoId;
+        try {
+            const delRes = await Video.findOneAndDelete({_id: videoId});
+            const myResponse = {
+                status: "deleted",
+                video: delRes
+            }
+            res.json(myResponse);
+        } catch (error) {
+            res.status(500).json({message: error.message});
+        }
     }
 
 }
